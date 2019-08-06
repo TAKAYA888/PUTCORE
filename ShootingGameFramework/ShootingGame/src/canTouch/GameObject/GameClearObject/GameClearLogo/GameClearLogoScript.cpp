0b190bcda2172db9c433fcf1d2245278589a7980
takaya_kanamori <last.takaya888@gmail.com>
2019-08-06 15:27:19 +0900

    すみません、画像を消す処理を忘れていたので［ゲームオーバー］と［ゲームクリア］に追加しました（金森）

--- ShootingGameFramework/ShootingGame/src/canTouch/GameObject/GameClearObject/GameClearLogo/GameClearLogoScript.cpp
+++ ShootingGameFramework/ShootingGame/src/canTouch/GameObject/GameClearObject/GameClearLogo/GameClearLogoScript.cpp
@@ -10,5 +10,8 @@ void GameClearLogoScript::update()
 
 void GameClearLogoScript::handleMessage(int eventMessageType, SafetyVoidSmartPtr<std::weak_ptr> param)
 {
-	
+	if (eventMessageType == DIE_TITLE_OBJECT)
+	{
+		getGameObject().lock()->destroy();
+	}
 }
--- ShootingGameFramework/ShootingGame/src/canTouch/GameObject/GameOverObject/GameOverLogo/GameOverLogoScript.cpp
+++ ShootingGameFramework/ShootingGame/src/canTouch/GameObject/GameOverObject/GameOverLogo/GameOverLogoScript.cpp
@@ -10,4 +10,8 @@ void GameOverLogoScript::update()
 
 void GameOverLogoScript::handleMessage(int eventMessageType, SafetyVoidSmartPtr<std::weak_ptr> param)
 {
+	if (eventMessageType == DIE_TITLE_OBJECT)
+	{
+		getGameObject().lock()->destroy();
+	}
 }
