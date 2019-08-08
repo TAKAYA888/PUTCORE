72ab680862b25745b15a037b40c03d6afb28277b
Yuki_Ishida <yuuki_120606@yahoo.co.jp>
2019-08-08 14:42:14 +0900

    Revert "test"
    This reverts commit 1fcc98667d6af2a5c1d8878b268b8327d58a1c1f.

--- ShootingGameFramework/ShootingGame/src/canTouch/GameObject/TestObject/Player/Player.h
+++ ShootingGameFramework/ShootingGame/src/canTouch/GameObject/TestObject/Player/Player.h
@@ -1,3 +1,11 @@
+#pragma once
+
+#include <TktkIncludePaths.h>
+#include "ProjectIncludePaths.h"
+#ifndef _MBCS
+/*F12を押してジャンプ-> */TktkManual;
+/*F12を押してジャンプ-> */ProjectIncludePaths;
+#endif
 
 #pragma once
 
