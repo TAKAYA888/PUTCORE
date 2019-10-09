b085e9620a0d4ab52bf162962cd1d39baf51e503
Yuki_Ishida <yuuki_120606@yahoo.co.jp>
2019-10-09 12:53:09 +0900

    とりあえずイージーモードテスト実装（石田）

---
+++ ShootingGameFramework/ShootingGame/src/canTouch/GameObject/LoadObject/ModeManager/ModeManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <TktkIncludePaths.h>
+#include "ProjectIncludePaths.h"
+#ifndef _MBCS
+/*F12を押してジャンプ-> */TktkManual;
+/*F12を押してジャンプ-> */ProjectIncludePaths;
+#endif
+
+#include "ModeManagerScript.h"
+
+struct ModeManager
+{
+	static void create()
+	{
+		// ゲームオブジェクトを作成
+		auto ModeManager = std::make_shared<GameObject>();
+
+		// ２次元座標
+		ModeManager->addComponent(std::make_shared<Transform2D>(
+			Vector2::zero,				// 座標（引数から受け取る）
+			Vector2(1.0f, 1.0f),	// スケール（倍率）
+			0.0f					// 回転（度数法）
+			));
+
+		// プレイヤーのスクリプト
+		ModeManager->addComponent(std::make_shared<ModeManagerScript>(
+			50.0f	// 移動速度
+			));
+
+		// ゲームオブジェクトを追加
+		GameObjectManager::addGameObject(ModeManager);
+	}
+};
